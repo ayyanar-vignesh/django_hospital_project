a2c954db9f969d211fbc8d876743608f707d2c16
AYYANAR VIGNESH <67584044+ayyanar-vignesh@users.noreply.github.com>
2023-05-11 11:08:34 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,47 @@
+#python operators
+
+
+# 1.arithmetic operator 
+# 2.assignment ...
+# 3.comparison...
+# 4.logical ...
+# 5.identity...
+# 6.membership...
+# 7.bitwise....
+
+
+#1 .arithmetic  
+
+#2.assignment 
+
+#3.comparison
+
+#4.logical ...
+
+# and ->if both conditions are true,return true
+
+# or ->if any one of the conditions are true,return true
+
+# not->reverse the reusult
+
+#5.identity 
+
+# is 
+
+# is not
+
+#6.membership 
+
+# in 
+
+# not in
+
+
+#7.bitwise 
+
+# & -and 
+
+# | -or 
+
+# ~ -not
+
